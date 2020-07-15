5fd13a38cb39a81531215eae8b29cadb6f771c9f
Jimmy Chu <jimmychu0807@gmail.com>
2020-07-15 17:05:22 +0800

    Added script for initializing the chain

--- chain-story-init/src/script.js
+++ chain-story-init/src/script.js
@@ -1,121 +1,154 @@
 import { ApiPromise, WsProvider, Keyring } from '@polkadot/api';
 import { blake2AsHex } from '@polkadot/util-crypto';
-const fs = require('fs');
+import config from './config/index';
 
-// config
-const WEB_SOCKET = 'ws://localhost:9944';
+// App constant
 const ALICE = '//Alice'
+const ALICE_ADDR = '5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY'
 
-// For custom type to be used to connect to the substrate chain
-const types = {
-  Address: "AccountId",
-  LookupSource: "AccountId"
-};
-
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
+const PRODUCTS = {
+  CANNED_TUNA: {
+    id: "0x3030303132333435363030303132", // GTIN-14: 00012345600012
+    owner: ALICE_ADDR,
+    props: [[
+      "0x64657363", // "desc"
+      "0x70726f647563743a63616e6e65642d74756e61", // "product:canned-tuna"
+    ]],
+  },
+  PINEAPPLE_PACKAGE: {
+    id: "0x3137333530303533383530303136", // GTIN-14: 17350053850016
+    owner: ALICE_ADDR,
+    props: [[
+      "0x64657363", // "desc"
+      "0x7061636b6167653a70696e656170706c65", // "package:pineapple"
+    ]],
+  }
 }
 
-const bufferToDigest = fileReader => {
-  // Turns the file content to a hexadecimal representation.
-  const content = Array.from(new Uint8Array(fileReader.result))
-    .map((b) => b.toString(16).padStart(2, '0'))
-    .join('');
-
-  return blake2AsHex(content, 256);
+const NEW_SHIPMENTS = {
+  TUNA_N_PINEAPPLE: {
+    id: "0x5330303031", // "S0001"
+    owner: ALICE_ADDR,
+    products: ["0x3030303132333435363030303132", "0x3137333530303533383530303136"]
+  }
 };
 
-const parseUserDocs = (res) => {
-  const ret = res
-    .map(row => `${row[1].toString()} => ("${String.fromCharCode(...row[2].toU8a())}", ${row[0].toNumber()})`)
-    .join(', ');
-
-  return `{ ${ret} }`;
+const SHIPMENT_EVS = {
+  PICKUP: {
+    id: "0x534530303031", // "SE0001"
+    event_type: "ShipmentPickup",
+    shipment_id: NEW_SHIPMENTS.TUNA_N_PINEAPPLE.id,
+    location: {
+      latitude: 5250186,
+      longitude: 5250186,
+    },
+    readings: null,
+    timestamp: 1593586800, // 20200701-15:00:00 GMT+8
+  },
+  TRANSIT_01: {
+    id: "0x534530303032", // "SE0002"
+    event_type: "ShipmentInTransit",
+    shipment_id: NEW_SHIPMENTS.TUNA_N_PINEAPPLE.id,
+    location: {
+      latitude: 7777777,
+      longitude: 7777777,
+    },
+    readings: null,
+    timestamp: 1593673200, // 20200702-15:00:00 GMT+8
+  },
+  TRANSIT_02: {
+    id: "0x534530303033", // "SE0003"
+    event_type: "ShipmentInTransit",
+    shipment_id: NEW_SHIPMENTS.TUNA_N_PINEAPPLE.id,
+    location: {
+      latitude: 8888888,
+      longitude: 8888888,
+    },
+    readings: null,
+    timestamp: 1593689400, // 20200702-19:30:00 GMT+8
+  },
+  DELIVERED: {
+    id: "0x534530303034", // "SE0004"
+    event_type: "ShipmentDelivered",
+    shipment_id: NEW_SHIPMENTS.TUNA_N_PINEAPPLE.id,
+    location: {
+      latitude: 9999999,
+      longitude: 9999999,
+    },
+    readings: null,
+    timestamp: 1593736200, // 20200703-08:30:00 GMT+8
+  }
 };
+
+const SENSOR_READINGS = {};
+
+const SLEEP_SEC = 8;
+const LAST_SLEEP_SEC = SLEEP_SEC + 5;
+
+function sleep(sec) {
+  console.log(`sleep for ${sec} sec...`);
+  return new Promise(resolve => setTimeout(resolve, sec * 1000));
+}
+
+let _nonce = 0;
+function getNextNonce() {
+  let now = _nonce;
+  _nonce += 1;
+  return now;
+}
 
 async function connect() {
   // Construct
-  const wsProvider = new WsProvider(WEB_SOCKET);
-  const api = await ApiPromise.create({ provider: wsProvider, types });
+  const wsProvider = new WsProvider(config.PROVIDER_SOCKET);
+  const api = await ApiPromise.create({ provider: wsProvider, types: config.CUSTOM_TYPES });
   return api;
 }
 
-async function submitDocInfo(filePath, comment) {
-  console.debug(`submitDocInfo: ${filePath}, ${comment}`);
+const sendTxBy = signer => async (subject, tx, txParam, opts = { wrapAndSpread: true }) => {
+  console.log(subject);
+  const txAndParams = opts.wrapAndSpread ? tx(...Object.values(txParam)) : tx(txParam);
+  const unsub = await txAndParams.signAndSend(signer, { nonce: getNextNonce() }, res => {
+      console.log(`${subject}: status: ${res.status}`);
+      res.status.isFinalized && unsub();
+    });
+  await sleep(SLEEP_SEC);
+};
+
+async function initChainStory() {
+  console.debug('Initiatizing chain story...');
+
   try {
     const api = await connect();
-
-    /******
-     * 学员们在这里追加逻辑
-     *
-     * 把 filePath 档档案通过 hash 函数算出它的 hash 值。然后和 comment 一起提交个 extrinsics
-     *   到 Substrate。
-     ******/
-
     const keyring = new Keyring({ type: 'sr25519' });
-    const content = fs.readFileSync(filePath, 'utf8');
-    const hash = bufferToDigest(content);
     const signer = keyring.addFromUri(ALICE);
+    console.log(`Signing by: ${ALICE}, addr: ${signer.address}`);
 
-    console.log(`hash: ${hash}`);
-    console.log(`signer: ${signer.address}`);
+    const sendTx = sendTxBy(signer);
 
-    api.tx.poeModule.createClaim(hash, comment)
-      .signAndSend(signer, (res) => {
-        if (!res.status.isFinalized) return;
-        console.log(`You have successfully claimed file with hash ${hash} with note "${comment}"`);
-      })
-      .catch(err => console.log(`error: ${err}`));
+    await sendTx('Create CANNED_TUNA',
+      api.tx.productRegistry.registerProduct, PRODUCTS.CANNED_TUNA);
 
-    // sleep for 20 seconds to wait for transaction result, wait for 3 blocks away x 6 secs / block.
-    await sleep(20000);
+    await sendTx('Create PINEAPPLE_PACKAGE',
+      api.tx.productRegistry.registerProduct, PRODUCTS.PINEAPPLE_PACKAGE);
+
+    await sendTx('Register a new shipment of TUNA_N_PINEAPPLE',
+      api.tx.productTracking.registerShipment, NEW_SHIPMENTS.TUNA_N_PINEAPPLE);
+
+    // Register all shipping events
+    for (const k in SHIPMENT_EVS) {
+      const ev = api.createType('ShippingEvent', SHIPMENT_EVS[k]);
+
+      await sendTx(`Shipment Event ${k}`,
+        api.tx.productTracking.recordEvent, ev, { wrapAndSpread: false });
+    }
 
   } catch (err) {
-    console.error(`Connect to Substrate error:`, err);
+    console.error(`Error init-chain-story:`, err);
     process.exit(1);
   }
 
+  await sleep(LAST_SLEEP_SEC);
   process.exit(0);
 }
 
-// Alice address: 5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY
-async function getUserDocs(acct) {
-  console.debug(`getUserDocs: ${acct}`);
-  try {
-    const api = await connect();
-
-    /******
-     * 学员们在这里追加逻辑
-     *
-     * 通过用户 addr, 取得他所有的创建文件的 hash及相关资料。返回值是：
-     * {
-     *   "0xabcd1234...": ["my note1", 3],
-     *   "0xabcd1235...": ["my note2", 5],
-     *   "0xabcd1236...": ["my note3", 7],
-     *   ...
-     * }
-     ******/
-    const res = await api.query.poeModule.usersDocs(acct);
-    console.log(parseUserDocs(res));
-  } catch (err) {
-    console.error(`Connect to Substrate error:`, err);
-  }
-
-  process.exit(0);
-}
-
-function main() {
-  const args = process.argv.slice(2, 5);
-  switch (args[0]) {
-    case 'submitDocInfo':
-      submitDocInfo(args[1], args[2])
-      break;
-    case 'getUserDocs':
-      getUserDocs(args[1]);
-      break;
-    default:
-      console.error('Unknown subcommand. Please use `submitDocInfo` or `getUserDocs` only.')
-  }
-}
-
-main();
+initChainStory();
