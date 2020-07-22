260f6ccca9f26e35bedb0f1f7bc5b79f46d5e9ee
Jimmy Chu <jimmychu0807@gmail.com>
2020-07-22 14:36:34 +0800

    UI further update and clearing all warnings

--- ui/src/components/ShipmentDetails.js
+++ ui/src/components/ShipmentDetails.js
@@ -1,14 +1,14 @@
-import React, { useEffect, useState, createRef } from 'react';
-import { Container, Header, Icon, Grid, List, Step, StepGroup } from 'semantic-ui-react';
+import React, { useEffect, useState } from 'react';
+import { Container, Header, Icon, Grid, List, Step } from 'semantic-ui-react';
 import 'semantic-ui-css/semantic.min.css';
-import { useSubstrate } from './substrate-lib';
+import { useSubstrate } from '../substrate-lib';
 
 function ShipmentDetailsComponent (props) {
   const { api } = useSubstrate();
-  const [ shipment, setShipment ] = useState({});
-  const [ eventIndices, setEventIndices] = useState([]);
-  const [ events, setEvents ] = useState([]);
-  const { accountPair, shipmentId } = props;
+  const [shipment, setShipment] = useState({});
+  const [eventIndices, setEventIndices] = useState([]);
+  const [events, setEvents] = useState([]);
+  const { shipmentId } = props;
 
   useEffect(() => {
     let unsubscribe;
@@ -20,11 +20,11 @@ function ShipmentDetailsComponent (props) {
     }
 
     if (shipmentId) {
-        shipment(shipmentId);
+      shipment(shipmentId);
     } else {
       return () => unsubscribe && unsubscribe();
     }
-  }, [api.query.productTracking]);
+  }, [api.query.productTracking, shipmentId]);
 
   useEffect(() => {
     let unsubscribe;
@@ -40,28 +40,30 @@ function ShipmentDetailsComponent (props) {
     } else {
       return () => unsubscribe && unsubscribe();
     }
-  }, [api.query.productTracking]);
+  }, [api.query.productTracking, shipmentId]);
 
   useEffect(() => {
     let unsubscribe;
 
     async function allEvents (eventIndices) {
       const futures = eventIndices
-        .map(async idx => await api.query.productTracking.allEvents(BigInt(idx)));
+        .map(idx => api.query.productTracking.allEvents(idx));
       Promise.all(futures)
         .then(data => {
-            if (data) {
-                const sorted = data.map(x => x.value).sort((a, b) => a.timestamp.toNumber() - b.timestamp.toNumber());
-                setEvents(sorted);
-            } else {
-                setEvents([]);
-            }
+          if (data) {
+            const sorted = data
+              .map(x => x.value)
+              .sort((a, b) => a.timestamp.toNumber() - b.timestamp.toNumber());
+            setEvents(sorted);
+          } else {
+            setEvents([]);
+          }
         })
         .catch(e => console.log(e));
     }
 
     if (eventIndices) {
-        allEvents(eventIndices);
+      allEvents(eventIndices);
     } else {
       return () => unsubscribe && unsubscribe();
     }
@@ -69,52 +71,49 @@ function ShipmentDetailsComponent (props) {
 
   return (
     <Container>
-        <Header as="h2">Shipment {shipmentId}</Header>
-        <Grid stackable columns='equal'>
-            <Grid.Column>
-                <Header as="h3">Status: Delivered</Header> 
-            </Grid.Column>
-        </Grid>
-        <Grid container columns={2} style={{ marginTop: '1em' }}>
-            <Grid.Column width={8}>
-                <Header as="h3">Shipping Events</Header>
-                { 
-                    events ?
-                        <Step.Group vertical size='small'>
-                            {events
-                                .map((event, idx) => {
-                                    const event_type = event.event_type.toString();
-                                    return (
-                                        <Step key={idx}>
-                                            <Icon name={event_type == 'ShipmentRegistration' ? 'tasks'
-                                                : event_type == 'ShipmentPickup' ? 'truck'
-                                                : event_type == 'ShipmentScan' ? 'barcode' : 'home'
-                                            } />
-                                            <Step.Content>
-                                                <Step.Title>{event.event_type.toString().substring(8)}</Step.Title>
-                                                <Step.Description>{new Date(event.timestamp.toNumber()).toLocaleString()}</Step.Description>
-                                            </Step.Content>
-                                        </Step>
-                                    );
-                            })
-                            }
-                        </Step.Group> : <div>No event found</div>
-                }
-            </Grid.Column>
-            <Grid.Column width={8}>
-                <Header as="h3">Products</Header>
-                { 
-                    shipment && shipment.products ?
-                        <List>
-                            {shipment.products
-                                .map((productId, idx) => {
-                                return <List.Item key={idx}>{productId.toString()}</List.Item >;
-                            })
-                            }
-                        </List > : <div>No event found</div>
-                }
-            </Grid.Column>
-        </Grid>
+      <Header as="h2">Shipment {shipmentId}</Header>
+      <Grid stackable columns='equal'>
+        <Grid.Column>
+          <Header as="h3">Status: Delivered</Header>
+        </Grid.Column>
+      </Grid>
+      <Grid container columns={2} style={{ marginTop: '1em' }}>
+        <Grid.Column width={8}>
+          <Header as="h3">Shipping Events</Header>
+          { events
+            ? <Step.Group vertical size='small'> { events.map((event, idx) => {
+              const eventType = event.event_type.toString();
+              return (
+                <Step key={idx}>
+                  <Icon name={ eventType === 'ShipmentRegistration'
+                    ? 'tasks'
+                    : eventType === 'ShipmentPickup'
+                      ? 'truck'
+                      : eventType === 'ShipmentScan'
+                        ? 'barcode' : 'home'
+                  } />
+                  <Step.Content>
+                    <Step.Title>{event.event_type.toString().substring(8)}</Step.Title>
+                    <Step.Description>
+                      { new Date(event.timestamp.toNumber()).toLocaleString() }
+                    </Step.Description>
+                  </Step.Content>
+                </Step>
+              );
+            })} </Step.Group>
+            : <div>No event found</div>
+          }
+        </Grid.Column>
+        <Grid.Column width={8}>
+          <Header as="h3">Products</Header>
+          { shipment && shipment.products
+            ? <List> { shipment.products.map((productId, idx) =>
+              <List.Item key={idx}>{ productId.toString() }</List.Item>
+            ) } </List>
+            : <div>No event found</div>
+          }
+        </Grid.Column>
+      </Grid>
     </Container>
   );
 }
