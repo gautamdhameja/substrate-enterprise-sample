fd50f232534f5ac12d3c1d7a65b1ba2f319921c0
Steve Degosserie <steve@parity.io>
2020-07-24 15:08:07 +0200

    Homepage content

--- ui/src/Homepage.js
+++ ui/src/Homepage.js
@@ -13,7 +13,8 @@ import {
   Responsive,
   Segment,
   Sidebar,
-  Visibility
+  Visibility,
+  List
 } from 'semantic-ui-react';
 
 const getWidth = () => {
@@ -26,7 +27,7 @@ const HomepageHeading = ({ mobile }) => (
   <Container text>
     <Header
       as='h1'
-      content='Substrate Supply Chain Demo'
+      content='Substrate Enterprise Sample'
       inverted
       style={{
         fontSize: mobile ? '2em' : '4em',
@@ -37,7 +38,7 @@ const HomepageHeading = ({ mobile }) => (
     />
     <Header
       as='h2'
-      content='Track your products at every step on your supply chain.'
+      content='Track products at every step on your supply chain.'
       inverted
       style={{
         fontSize: mobile ? '1.5em' : '1.7em',
@@ -196,16 +197,44 @@ const Homepage = () => (
         <Grid.Row>
           <Grid.Column width={8}>
             <Header as='h3' style={{ fontSize: '2em' }}>
-              What is this demo about?
+              What is this sample about?
             </Header>
             <p style={{ fontSize: '1.33em' }}>
-              TODO TODO TODO
+            The <strong>Substate Enterprise Sample</strong> demonstrates how a software engineering team can quickly build an <strong>application-specific Blockchain</strong> and related applications, by leveraging the <strong>Parity Substrate framework</strong> and its standard <strong>FRAME library</strong>, as well as building composable runtime modules (known as pallets).
+            </p>
+            <p style={{ fontSize: '1.33em' }}>
+            The sample implements, in a simplified manner, a Blockchain-based solution for a collaborative supply-chain ecosystem.
+            </p>
+            <p style={{ fontSize: '1.33em' }}>
+            Among others, it allows to:
+            </p>
+            <p style={{ fontSize: '1.33em' }}>
+              <List bulleted>
+                <List.Item content="Setup a shared platform among several organisations, as a permissioned Blockchain network."/>
+                <List.Item content="Manage decentralized identities for member organisations and their delegates."/>
+                <List.Item content="Register master data about products and their owning organisations."/>
+                <List.Item content="Register and track a shipment's journey through the value chain."/>
+                <List.Item content="Monitor a shipment's storage and transportation conditions."/>
+                <List.Item content="Enable seamless data integration with existing ERP systems deployed within corporate walls." />
+              </List>
+            </p>
+            <p style={{ fontSize: '1.33em' }}>Specific features of the <strong>Parity Substrate framework</strong> exhibited:</p>
+            <p style={{ fontSize: '1.33em' }}>
+              <List bulleted>
+                <List.Item content="Consortium network with a Proof-of-Authority consensus (Aura for block production, GRANDPA for block finalization)."/>
+                <List.Item content="Dynamic set of authority nodes."/>
+                <List.Item content="Enterprise-class role-based access control."/>
+                <List.Item content="W3C Decentralized Identifiers (DIDs) for organizations & delegates."/>
+                <List.Item content="Substrate Node & Frontend starter templates."/>
+                <List.Item content="Unrestricted flexibility for runtime logic & storage in custom pallets e.g. product registry & tracking pallets."/>
+                <List.Item content="Reliable off-chain data integration with Offchain workers."/>
+              </List>
             </p>
           </Grid.Column>
         </Grid.Row>
 
         <Divider as='h4' className='header' horizontal
-          style={{ margin: '3em 0em', textTransform: 'uppercase' }}
+          style={{ margin: '1em 0em', textTransform: 'uppercase' }}
         />
 
         <Grid.Row>
@@ -214,22 +243,36 @@ const Homepage = () => (
               Related Github Repositories
             </Header>
             <p style={{ fontSize: '1.33em' }}>
-              TODO TODO TODO
+              <List bulleted>
+                <List.Item content="Parity Substrate framework" href="https://github.com/paritytech/substrate"/>
+                <List.Item content="Polkadot Javascript libraries" href="https://github.com/polkadot-js/"/>
+                <List.Item content="Substrate Enterprise sample" href="https://github.com/gautamdhameja/substrate-enterprise-sample" />
+                <List.Item content="Substrate Frontend template" href="https://github.com/substrate-developer-hub/substrate-front-end-template"/>
+                <List.Item content="Substrate Node template" href="https://github.com/substrate-developer-hub/substrate-node-template" />
+                <List.Item content="DID pallet" href="https://github.com/substrate-developer-hub/pallet-did"/>
+                <List.Item content="Product Registry pallet" href="https://github.com/stiiifff/pallet-product-registry"/>
+                <List.Item content="Product Tracking pallet" href="https://github.com/stiiifff/pallet-product-tracking"/>
+                <List.Item content="RBAC pallet" href="https://github.com/gautamdhameja/substrate-rbac"/>
+                <List.Item content="Validator Set pallet" href="https://github.com/gautamdhameja/substrate-validator-set"/>
+              </List>
             </p>
           </Grid.Column>
         </Grid.Row>
 
         <Divider as='h4' className='header' horizontal
-          style={{ margin: '3em 0em', textTransform: 'uppercase' }}
+          style={{ margin: '1em 0em', textTransform: 'uppercase' }}
         />
 
         <Grid.Row>
           <Grid.Column width={8}>
             <Header as='h3' style={{ fontSize: '2em' }}>
-              Contact Us
+              Get Started !
             </Header>
             <p style={{ fontSize: '1.33em' }}>
-              TODO TODO TODO
+              <List>
+                <List.Item content="Substrate Developer Hub" href="https://substrate.dev"/>
+                <List.Item content="Ask Questions" href="https://riot.im/app/#/room/!HzySYSaIhtyWrwiwEV:matrix.org"/>
+              </List>
             </p>
           </Grid.Column>
         </Grid.Row>
