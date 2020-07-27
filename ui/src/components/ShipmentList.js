21345031545a9df08aef1666b113ca01bb2ff765
Jimmy Chu <jimmychu0807@gmail.com>
2020-07-27 13:55:02 +0800

    spacing fix

--- ui/src/components/ShipmentList.js
+++ ui/src/components/ShipmentList.js
@@ -4,54 +4,52 @@ import { useSubstrate } from '../substrate-lib';
 import { hexToString } from '@polkadot/util';
 
 export default function Main (props) {
-    const { api } = useSubstrate();
-    const { accountPair, setSelectedShipment } = props;
-    const [shipments, setShipments] = useState([]);
-    const [selected, setSelected] = useState('');
+  const { api } = useSubstrate();
+  const { accountPair, setSelectedShipment } = props;
+  const [shipments, setShipments] = useState([]);
+  const [selected, setSelected] = useState('');
 
-    useEffect(() => {
-      let unsub = null;
+  useEffect(() => {
+    let unsub = null;
 
-      async function shipments (accountPair) {
-        const addr = accountPair.address;
-        unsub = await api.query.productTracking.shipmentsOfOrganization(addr, data => {
-          setShipments(data);
-          setSelectedShipment('');
-          setSelected('');
-        });
-      }
-
-      if (accountPair) shipments(accountPair);
-      return () => unsub && unsub();
-    },
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    [accountPair, api.query.productTracking]);
-
-    const handleSelectionClick = (ev, { data }) => {
-      const shipment = hexToString(shipments[data].toString());
-      setSelectedShipment(shipment);
-      setSelected(shipment);
-    };
-
-    if (!shipments || shipments.length === 0) {
-      return <Message warning>
-        <Message.Header>No shipment registered for your organisation.</Message.Header>
-        <p>Please create one using the above form.</p>
-      </Message>;
+    async function shipments (accountPair) {
+      const addr = accountPair.address;
+      unsub = await api.query.productTracking.shipmentsOfOrganization(addr, data => {
+        setShipments(data);
+        setSelectedShipment('');
+        setSelected('');
+      });
     }
 
-    return <Card fluid color = 'blue'>
-      <Card.Content style={{ flexGrow: 0 }} header = 'Shipment List' />
-      <Card.Content>
-        <Card.Description>{ shipments
-          ? <List selection>
+    if (accountPair) shipments(accountPair);
+    return () => unsub && unsub();
+  }, [accountPair, api.query.productTracking, setSelectedShipment]);
+
+  const handleSelectionClick = (ev, { data }) => {
+    const shipment = hexToString(shipments[data].toString());
+    setSelectedShipment(shipment);
+    setSelected(shipment);
+  };
+
+  if (!shipments || shipments.length === 0) {
+    return <Message warning>
+      <Message.Header>No shipment registered for your organisation.</Message.Header>
+      <p>Please create one using the above form.</p>
+    </Message>;
+  }
+
+  return <Card fluid color = 'blue'>
+    <Card.Content style={{ flexGrow: 0 }} header = 'Shipment List' />
+    <Card.Content>
+      <Card.Description>{ shipments
+        ? <List selection>
           { shipments.map((shipment, idx) => {
             const shipmentId = hexToString(shipment.toString());
             return <List.Item key={idx} active={selected === shipmentId} header={shipmentId}
-            onClick={handleSelectionClick} data={idx}/>;
+              onClick={handleSelectionClick} data={idx}/>;
           }) }</List>
-          : <div>No shipment found</div>
-        }</Card.Description>
-      </Card.Content>
-    </Card>;
+        : <div>No shipment found</div>
+      }</Card.Description>
+    </Card.Content>
+  </Card>;
 }
