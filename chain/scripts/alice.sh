51d1b04040943f4ebb02ae9b3e1b19331fb78427
Steve Degosserie <steve@parity.io>
2020-09-29 11:10:30 +0200

    Some handy scripts to start a 3-node (2 validators, 1 full node) local network

---
+++ chain/scripts/alice.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+./target/release/enterprise-sample --base-path /tmp/alice --chain=local --alice --validator --node-key 0000000000000000000000000000000000000000000000000000000000000001 --unsafe-ws-external
---
+++ chain/scripts/charlie.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+./target/release/enterprise-sample --base-path /tmp/charlie --chain=local --charlie --port 30334 --bootnodes /ip4/127.0.0.1/tcp/30333/p2p/12D3KooWEyoppNCUx8Yx66oV9fJnriXwCcXwDDUA2kj6vnc6iDEp
